809129554c29be41929885a56754c8e241498a37
bulletpal <jacob.w.gable@gmail.com>
2018-07-08 19:16:30 -0500

    Update access.php

--- access.php
+++ access.php
@@ -1,4 +1,5 @@
 <?php
     $mysqli = new mysqli("den1.mysql5.gear.host", "gableon01", "Bd51!2-23t6b", "gableon01");
+    $mysqli->query("INSERT INTO messages VALUES ('bulletpal', 'Hello!');
     $result = $mysqli->query("SELECT * FROM messages");
 ?>
