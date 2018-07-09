719bb2049b1c61aad31010798c8b2e1d5e8b3569
bulletpal <jacob.w.gable@gmail.com>
2018-07-08 22:04:03 -0500

    Update access.php

--- access.php
+++ access.php
@@ -1,8 +1,10 @@
 <?php
+if($_GET['debug'] == "true"){
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+}
+    
 $servername = "den1.mysql5.gear.host";
 $username = "gableon01";
 $password = $_GET['pw'];
@@ -28,6 +30,8 @@ function collectData($limit){
 
 if($_GET['un'] == null){
     echo "NULL";
+} else {
+    echo $_GET['un']
 }
 
 $conn->close();
