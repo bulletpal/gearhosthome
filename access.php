6f01ce3aeafada63cf29ede920746f62a49839f5
bulletpal <jacob.w.gable@gmail.com>
2018-07-08 21:24:02 -0500

    Update access.php

--- access.php
+++ access.php
@@ -22,7 +22,7 @@ $result = $conn->query("SELECT * FROM messages");
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo $row["screenname"]. ", " . $row["Message"]. "<br>";
+        echo $row["id"]. ", ". $row["screenname"]. ", " . $row["Message"]. "<br>";
     }
 } else {
     echo "0 results";
