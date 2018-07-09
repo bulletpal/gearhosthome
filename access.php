dfa3c98577f0edeca90bb353f43ff78f767659ee
bulletpal <jacob.w.gable@gmail.com>
2018-07-08 20:07:27 -0500

    Update access.php

--- access.php
+++ access.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $servername = "den1.mysql5.gear.host";
 $username = "gableon01";
 $password = "Bd51!2-23t6b";
@@ -12,13 +16,16 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo $row["screenname"]. ", " . $row["Message"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
+$result = $connn->query("SELECT * FORM messages");
+echo $result;
+
+//if ($result->num_rows > 0) {
+//    // output data of each row
+//    while($row = $result->fetch_assoc()) {
+//        echo $row["screenname"]. ", " . $row["Message"]. "<br>";
+//    }
+//} else {
+//    echo "0 results";
+//}
 $conn->close();
 ?>
